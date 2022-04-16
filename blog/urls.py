2f8c2debb763c48e6f134428aec52d0c51ae0c00
Osahon (Sean) Irabor <osahonirabor@hotmail.com>
2022-04-15 17:29:05 -0700

    updated url settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path 
 from .import views
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
 
 app_name = 'blog'
 urlpatterns = [
     path("", views.blog_index, name='blog_index'),
     path("<int:pk>/", views.blog_detail, name='blog_detail'),
     path("<category>/", views.blog_category, name="blog_category"),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
 from .import views
-
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path("", views.project_index, name='project_index'),
     path("<int:pk>/", views.project_detail, name='project_detail'), 
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
