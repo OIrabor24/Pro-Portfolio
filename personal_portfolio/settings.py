0482c901a11b092a0150cc07d044f1582928a978
Osahon (Sean) Irabor <osahonirabor@hotmail.com>
2022-04-18 15:20:22 -0700

    trying to get s3 to work

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -148,12 +148,13 @@ DATABASES['default'].update(db_from_env)
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #AWS Setting for using S3 as CDN!
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') 
 AWS_QUERYSTRING_AUTH = False 
 AWS_S3_REGION_NAME = 'us-west-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
